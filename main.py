27e9fe1ac39764bcaf4469c59df8fe9a4ba0fbae
Jon Fairbanks <fairplay89@gmail.com>
2020-10-21 23:33:53 -0700

    Clean-up

--- main.py
+++ main.py
@@ -12,14 +12,14 @@ from tqdm import tqdm
 hashes = {}
 faceCascade = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_frontalface_default.xml")
 
-def fix_png(filename):
+def fix_png_data(filename):
     if filename.endswith('.png'):
         try:
             img_location = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__), 'images', filename))
             cmd = 'pngcrush -q -ow -rem allb -reduce "' + img_location + '" > /dev/null 2>&1'
             os.system(cmd)
         except Exception as e:
-            print(f'Error Fixing PNG: {filename}')
+            print(f'Error Fixing PNG: {filename} {e}')
             pass
 
 def detect_duplicates(filename):
@@ -45,7 +45,7 @@ def detect_duplicates(filename):
             hashes[current_hash] = filename
 
     except Exception as e:
-        print(f'Error Detecting Duplicate: {filename}')
+        print(f'Error Detecting Duplicate: {filename} {e}')
         pass
 
 def detect_faces(filename):
@@ -93,11 +93,13 @@ def main():
 
     for filename in tqdm(sorted(os.listdir('images'))):
         if filename.endswith('.png'):
-            fix_png(filename)
+            fix_png_data(filename)
         detect_duplicates(filename)
         detect_faces(filename)
-
+    
+    hashes = {}
     print("\nImage processing completed!\n")
+    return
 
 if __name__ == '__main__':
     if not os.path.exists('images'):
